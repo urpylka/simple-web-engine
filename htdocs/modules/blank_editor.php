b448b8c8f173a311e691cc31c6f03f3c8c3c0039
Artem Smirnov <urpylka@gmail.com>
2018-09-04 20:10:23 +0300

    move editor & fix him syntax

--- htdocs/modules/blank_editor.php
+++ htdocs/modules/blank_editor.php
@@ -2,10 +2,10 @@
 	$page_link=$_POST['moo_link'];//принимаем переменную
 	//echo $page_link;
 	//echo $_POST['moo_text'];
-	if($page_link!=NULL)
+	if($page_link != NULL)
 	{
-		$page_sql=mysql_query("SELECT id FROM pages WHERE link='".$page_link."';");
-		$count=mysql_num_rows($page_sql);
+		$page_sql = mysql_query("SELECT id FROM pages WHERE link='".$page_link."';");
+		$count = mysql_num_rows($page_sql);
 		switch($count){
 			case '0':
 				throw new Exception('По данному запросу не найдено страниц в базе данных.');
@@ -13,7 +13,7 @@
 				$view_page_info = mysql_fetch_assoc($page_sql);
 				$id = $view_page_info['id'];
 				//изменяем контент страницы
-				$page_sql=mysql_query("UPDATE `pages` SET `text` = '".$_POST['moo_text']."' WHERE `id` = ".$id.";");
+				$page_sql = mysql_query("UPDATE `pages` SET `text` = '".$_POST['moo_text']."' WHERE `id` = ".$id.";");
 				echo "<p style='margin-left:30px;'>Страница сохранена!</p>";
 				break;
 			default:
