ae94f743ae00851997dc65ccd5b7782b46cb27f6
Artem Smirnov <urpylka@gmail.com>
2018-09-04 22:41:08 +0300

    fixes

--- htdocs/modules/blank_editor.php
+++ htdocs/modules/blank_editor.php
@@ -29,25 +29,19 @@
 		$page_by_link = $db->prepare("SELECT id FROM pages WHERE link = :moo_link");
 		$page_by_link->bindValue(':moo_link', $moo_link, PDO::PARAM_STR);
 		$page_by_link->execute();
-		//$page_by_link = $page_by_link->FETCH(PDO::FETCH_ASSOC);
 
 		if ($DEBUG)
 		{
 			var_dump($page_by_link);
 	    	echo "rowCount: ".$page_by_link->rowCount();
-			echo "id: ".$page_by_link['id'];
 		}
 
-		$page_sql = mysql_query("SELECT id FROM pages WHERE link = '".$moo_link."';");
-
-
-		$count = mysql_num_rows($page_sql);
-		switch($count){
+		switch($page_by_link->rowCount()){
 			case '0': throw new Exception('No pages were found in the database for this query.');
 			case '1':
-				$view_page_info = mysql_fetch_assoc($page_sql);
-				$id = $view_page_info['id'];
-				$page_sql = mysql_query("UPDATE `pages` SET `text` = '".$_POST['moo_text']."' WHERE `id` = ".$id.";");
+				echo $page_by_link->FETCH(PDO::FETCH_ASSOC)['id'];
+				//$id = $view_page_info['id'];
+				//$page_sql = mysql_query("UPDATE `pages` SET `text` = '".$_POST['moo_text']."' WHERE `id` = ".$id.";");
 				echo "<p style='margin-left:30px;'>The page was saved!</p>";
 				break;
 			default: throw new Exception('$count pages have been returned for this request, but there must be one.');
