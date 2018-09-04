468572ec5368edaaa8e5cb8d786373c8ff0c7ed9
Artem Smirnov <urpylka@gmail.com>
2018-09-04 17:55:51 +0300

    move config & move connect to page.php

---
+++ htdocs/config.php
@@ -0,0 +1,11 @@
+﻿<?
+    // Site settings
+    $site_url = "http://war.ssau.ru";
+    $site_name = "Военная кафедра";
+
+    // Database settings
+    $host = "localhost";
+    $login_mysql = "root";
+    $password_mysql = "PASSWD";
+    $database = "my_war";
+?>
--- htdocs/page.php
+++ htdocs/page.php
@@ -7,6 +7,12 @@ header('Content-Type: text/html; charset=utf-8');
 ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
 include("modules/config.php");
+
+$db = @mysql_connect("$host", "$login_mysql", "$password_mysql"); 
+if (!$db) exit("<center><p class=\"error\">К сожалению, не доступен сервер MySQL</p></center>"); 
+if (!@mysql_select_db($database,$db)) exit("<center><p class=\"error\">К сожалению, не доступна база данных</p></center>");
+mysql_set_charset('utf8',$db);
+
 /*
 $page_id = $_GET['id'];//принимаем переменную
 if($page_id!=NULL)
