aeebba95702173e9f939059edc21d2f156e01d52
Artem Smirnov <urpylka@gmail.com>
2018-09-07 23:27:44 +0300

    fix main menu

--- htdocs/modules/block_main_menu.php
+++ htdocs/modules/block_main_menu.php
@@ -62,18 +62,19 @@ $list_menu->bindValue(':admin_flag', $admin_flag, PDO::PARAM_INT);
 $list_menu->execute();
 
 while ( $root_item = $list_menu->FETCH(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT) ) {
-	$list_menu->bindValue(':root_id', $root_item['id'], PDO::PARAM_INT);
-	$list_menu->bindValue(':admin_flag', $admin_flag, PDO::PARAM_INT);
-	$list_menu->execute();
+	$inner_list_menu = $list_menu;
+	$inner_list_menu->bindValue(':root_id', $root_item['id'], PDO::PARAM_INT);
+	$inner_list_menu->bindValue(':admin_flag', $admin_flag, PDO::PARAM_INT);
+	$inner_list_menu->execute();
 
-	if ( $list_menu->rowCount() == 0 ) { echo "<li><a href=\"".$root_item['link']."\">".$root_item['name']."</a></li>"; }
+	if ( $inner_list_menu->rowCount() == 0 ) { echo "<li><a href=\"".$root_item['link']."\">".$root_item['name']."</a></li>"; }
 	else {
 		echo "<li class='toggle'>";
 		echo "<a href=\"".$root_item['link']."\">".$root_item['name']."</a>";
 		$root_item['id'] = ( $root_item['id'] == 1 ) ? '0' : $root_item['id'];
 		echo "<ul>";
 
-		while ( $inner_item = $list_menu->FETCH(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT) ) {
+		while ( $inner_item = $inner_list_menu->FETCH(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT) ) {
 			echo "<li><a href=\"".$inner_item['link']."\">".$inner_item['name']."</a></li>";
 		}
 		echo "</ul>";
