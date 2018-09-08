fda01ce35c3bf3b657f7d4f5912c81ac0cd5b12c
Artem Smirnov <urpylka@gmail.com>
2018-09-08 03:06:07 +0300

    new login kernel
    add regist, login, logout (use pbkdf2)

--- htdocs/modules/block_page_login_form.php
+++ htdocs/modules/block_page_login_form.php
@@ -2,69 +2,136 @@
 	<section class="text-content" id="fullpage">
 		<h1><?=$page_title?></h1>
 		<?
-		// ВЫХОД
-		if ( ( isset($_GET['action']) && $_GET['action'] == "logout" ) && isset($_SESSION['login']) )
-		{
-			session_unset();
-			session_destroy();
-		}
-		// АВТОРИЗАЦИЯ
-		if(isset($_POST['login']) && isset($_POST['password']) && !isset($_SESSION['login']))
-		{
-			$users = mysql_query("
-			SELECT * FROM users
-			WHERE login = '". $_POST['login']."'
-			AND password = '".md5($_POST['password'])."';");
-			// если найдена хоть одна строка (admins - group_id='1')
-			if(mysql_numrows($users)=="1")
-			{
-				// регистрируем сессионные переменные
-				$_SESSION["login"] = $_POST['login'];
-				$_SESSION["password"] = $_POST['password'];
-				$_SESSION["group_id"] = mysql_fetch_assoc($users)['group_id'];
-				//if (!$_SESSION["password"]) die("Session var passsword not registered");
-				echo("<div>Вы успешно авторизованы! ".$_SESSION['login'].":".$_SESSION['group_id']."</div>");
-				echo ("<div>".session_name().' = '.session_id()."</div>");
-				//print_r($_GET);
-				echo(isset($_GET['refer'])?"<div>Нажмите <a href='".$_GET['refer']."'>сюда</a>, чтобы вернуться на предущую страницу.</div>":"");
-				?>
-					<div><a href="login?action=logout">Выйти</div>
-				<?
-				/*
-				здесь надо проверку с какого адреса пришел и если это этот сайт то (ссылку на переход на предыдущую страницу)
-				*/
+		if ( isset($_GET['act']) ) {
+			switch ($_GET['act']) {
+				case "logout":
+				if ( ! isset($login) ) { echo "<p>Ошибка! Вы не можете выйти тк не еще залогинены.</p>"; }
+				else {
+					session_unset();
+					session_destroy();
+					// по идеи надо сделать удаление из БД
+				}
+				break;
+				case "login":
+				if ( isset($_POST['login']) && isset($_POST['password']) ) {
+					if ( isset($login) ) {
+						echo "<p>Ошибка! Вы не можете залогиниться, тк уже авторизованы под логином: ".$login."</p>";
+					}
+					else {
+						// получить (по логину) pbkdf2 правило для проверки пароля по хешу
+						// https://docs.djangoproject.com/en/2.1/topics/auth/passwords/
+						// http://php.net/manual/en/function.hash-pbkdf2.php
+	
+						$pbkdf2_by_login = $pdo->prepare("SELECT `users`.`pbkdf2` FROM `users` WHERE `users`.`login` = :login;");
+						$pbkdf2_by_login->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
+						$pbkdf2_by_login->execute();
+						$count_pbkdf2 = $pbkdf2_by_login->rowCount();
+	
+						switch($count_pbkdf2) {
+							case '0':
+								?>
+								<div>Вы ввели неправильный логин или пароль!</div>
+								<form method="post" action="login?act=login">
+									<input type="text" name="login" value="" onclick="if(this.value=='')this.value='';" onblur="if(this.value=='')this.value='';" />
+									<input type="password" name="password" value="" onclick="if(this.value=='')this.value='';" onblur="if(this.value=='')this.value='';" />
+									<input type="submit" value="Войти" />
+								</form>
+								<?
+								break;
+							case '1':
+								// <algorithm>$<iterations>$<salt>$<hash>
+								$pbkdf2 = explode('$', $pbkdf2_by_login->FETCH(PDO::FETCH_ASSOC)['pbkdf2']);
+	
+								// проверить пароль
+								if ( $pbkdf2['4'] != hash_pbkdf2($pbkdf2['1'], $_POST['password'], $pbkdf2['3'], $pbkdf2['2'], 20) ) {
+									?>
+									<div>Вы ввели неправильный логин или пароль!</div>
+									<form method="post" action="login?act=login">
+										<input type="text" name="login" value="" onclick="if(this.value=='')this.value='';" onblur="if(this.value=='')this.value='';" />
+										<input type="password" name="password" value="" onclick="if(this.value=='')this.value='';" onblur="if(this.value=='')this.value='';" />
+										<input type="submit" value="Войти" />
+									</form>
+									<?
+								}
+								else {
+									// в случае успеха присвоить сессии user_id
+									$pbkdf2_by_login = $pdo->prepare("INSERT INTO `sessions` VALUES(':session_id',':login');");
+									$pbkdf2_by_login->bindValue(':session_id', session_id(), PDO::PARAM_STR);
+									$pbkdf2_by_login->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
+									if ( $pbkdf2_by_login->execute() )
+									{
+										echo("<div>Вы успешно авторизованы! ".$_POST['login']."</div>");
+										echo(isset($_GET['refer'])?"<div>Нажмите <a href='".$_GET['refer']."'>сюда</a>, чтобы вернуться на предущую страницу.</div>":"");
+										?>
+										<div><a href="login?act=logout">Выйти</div>
+										<?
+									}
+									else { echo "<p>Ошибка! Не удалось привязать сессию к пользователю.</p>"; }
+								}
+								break;
+							default:
+								echo "<p>ERROR: Системная ошибка. По запросу логина найдено ".$count_pbkdf2." записей!</p>";
+								break;
+						}
+					}
+				}
+				// else echo "Вы не ввели логин или пароль"
+				break;
+				case "registr":
+				if ( isset($login) ) { echo "<p>Ошибка! Вы не можете зарегистрироваться тк уже авторизованы под логином: ".$login.".</p>"; }
+				else {
+					if ( isset($_POST['login']) && isset($_POST['password']) ) {
+
+						// проверяем существует ли пользователь
+						$user_exist = $pdo->prepare("SELECT COUNT(*) FROM `users` WHERE `users`.`login` = :login;");
+						$user_exist->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
+						$user_exist->execute();
+
+						if ( $user_exist->FETCH(PDO::FETCH_NUM)['0'] != 0 )
+						{ echo "<p>Ошибка! Имя пользователя занято</p>"; }
+						else {
+
+							// Generate a random IV using openssl_random_pseudo_bytes()
+							// random_bytes() or another suitable source of randomness
+							$salt = openssl_random_pseudo_bytes(16);
+							$password = $_POST['password'];
+							$iterations = 1000;
+							$hash = hash_pbkdf2("sha256", $password, $salt, $iterations, 20);
+							
+							// <algorithm>$<iterations>$<salt>$<hash>
+							$pbkdf2 = "sha256$".$iterations.'$'.$salt.'$'.$hash;
+
+							$user_add = $pdo->prepare("INSERT INTO `users` (`login`,`pbkdf2`) VALUES(':login',':pbkdf2');");
+							$pbkdf2_by_login->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
+							$pbkdf2_by_login->bindValue(':pbkdf2', $pbkdf2, PDO::PARAM_STR);
+							if ( $pbkdf2_by_login->execute() )
+							{ echo "<p>Пользователь ".$_POST['login']." успешно добавлен.</p>"; }
+							else  { echo "<p>Ошибка при добавлении пользователя ".$_POST['login'].".</p>"; }
+						}
+					}
+					// else echo "Вы не ввели логин или пароль"
+				}
+				break;
 			}
-			// Иначе выводим предупреждение и форму авторизации
-			else{
+			
+		}
+		else {
+			if ( isset($login) ) {
+				echo("<div>Вы успешно авторизованы! ".$_SESSION['login']."</div>");
 				?>
-				<div>Вы ввели неправильный логин или пароль!</div>
-				<form method="post" action="login">
+					<div><a href="login?act=logout">Выйти</a></div>
+				<?
+			}
+			else {
+				?>
+				<div>Введите учетные данные</div>
+				<form method="post" action="login?act=login<?echo(isset($_GET['refer'])?"&refer=".$_GET['refer']:"");?>">
 					<input type="text" name="login" value="" onclick="if(this.value=='')this.value='';" onblur="if(this.value=='')this.value='';" />
 					<input type="password" name="password" value="" onclick="if(this.value=='')this.value='';" onblur="if(this.value=='')this.value='';" />
 					<input type="submit" value="Войти" />
 				</form>
 				<?
 			}
-		}
-		// Иначе выводим форму авторизации
-		else if(!isset($_SESSION['login']))
-		{
-			?>
-			<div>Введите учетные данные</div>
-			<form method="post" action="login<?echo(isset($_GET['refer'])?"?refer=".$_GET['refer']:"");?>">
-				<input type="text" name="login" value="" onclick="if(this.value=='')this.value='';" onblur="if(this.value=='')this.value='';" />
-				<input type="password" name="password" value="" onclick="if(this.value=='')this.value='';" onblur="if(this.value=='')this.value='';" />
-				<input type="submit" value="Войти" />
-			</form>
-		<?}
-		else
-		{
-			//важным является то что сюда мы попадаем при повторной отправке данных
-			echo("<div>Вы успешно авторизованы! ".$_SESSION['login'].":".$_SESSION['group_id']."</div>");
-			echo ("<div>".session_name().' = '.session_id()."</div>");
-			?>
-				<div><a href="login?action=logout">Выйти</a></div>
-			<?
 		}
 		?>
 	</section>
