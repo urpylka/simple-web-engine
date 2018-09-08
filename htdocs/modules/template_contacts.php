98cfb53fde0df001a4a5c29f85a2888502b6d92f
Artem Smirnov <urpylka@gmail.com>
2018-09-08 06:30:12 +0300

    debug

--- htdocs/modules/template_contacts.php
+++ htdocs/modules/template_contacts.php
@@ -5,9 +5,20 @@
 	</header>
 	<main>
 		<article>
-			<section class="text-content" id="fullpage" style="display: table !important;">
-			<?=$page_content?>
+			<section class="text-content" id="fullpage">
+				<h1><?=$page_title?></h1>
+				<div id="main_cont">
+					<?=$page_content?>
+				</div>
 			</section>
 		</article>
+		<?
+			if ($admin_flag && ! $error_output)
+			{
+				$moo_text = $page_content;
+				$moo_link = $page_link;
+				include_once 'modules/module_redactor.php';
+			}
+		?>
 	</main>
 <? include_once 'modules/site_footer.php'; ?>
--- htdocs/modules/template_main.php
+++ htdocs/modules/template_main.php
@@ -8,7 +8,15 @@
 	</header>
 	<main>
 		<article>
-			<? include_once $page_content; ?>
+			<?=$page_content?>
 		</article>
+		<?
+			if ($admin_flag && ! $error_output)
+			{
+				$moo_text = $page_content;
+				$moo_link = $page_link;
+				include_once 'modules/module_redactor.php';
+			}
+		?>
 	</main>
 <? include_once 'modules/site_footer.php'; ?>
