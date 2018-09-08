16407d935d612e2cd072ff91397b7fe31f111b94
Smirnov Artem <urpylka@users.noreply.github.com>
2018-09-08 14:31:12 +0300

    fix emty login & add second check pbkdf2

--- htdocs/modules/block_page_login_form.php
+++ htdocs/modules/block_page_login_form.php
@@ -62,7 +62,7 @@
 							case '1':
 							// <algorithm>$<iterations>$<salt>$<hash>
 							$pbkdf2 = explode('$', $pbkdf2_by_login->FETCH(PDO::FETCH_ASSOC)['pbkdf2']);
-							if ( ! isset($pbkdf2['3']) ) { echo "<p>Системная ошибка! Неккоретный pbkdf2 в БД.</p>"; }
+							if ( ! isset($pbkdf2['3'] || isset($pbkdf2['4']) ) { echo "<p>Системная ошибка! Неккоретный pbkdf2 в БД.</p>"; }
 							else {
 								// проверить пароль
 								if ( $pbkdf2['3'] != hash_pbkdf2($pbkdf2['0'], $_POST['password'], $pbkdf2['2'], $pbkdf2['1'], 20) ) {
@@ -74,7 +74,7 @@
 									$pbkdf2_by_login = $pdo->prepare("INSERT INTO `sessions` (`login`,`phpsessid`) VALUES (:login, :session_id);");
 									$pbkdf2_by_login->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
 									$pbkdf2_by_login->bindValue(':session_id', session_id(), PDO::PARAM_STR);
-									if ( $pbkdf2_by_login->execute() ) { view_login(); }
+									if ( $pbkdf2_by_login->execute() ) { $login = $_POST['login']; view_login(); }
 									else { echo "<p>Ошибка! Не удалось привязать сессию к пользователю.</p>"; view_login_form(); }
 								}
 							}
