5c183f1dcea21a5af4c59364c0d0509b0783f395
Artem Smirnov <urpylka@gmail.com>
2018-09-08 06:05:00 +0300

    debug

--- htdocs/modules/template_main.php
+++ htdocs/modules/template_main.php
@@ -3,13 +3,13 @@
 		<? include_once 'modules/module_navbar.php'; ?>
 		<? include_once 'modules/block_promo.php'; ?>
 		<? include_once 'modules/block_quick-menu.php'; ?>
+		<hr/>
+		<? include_once 'modules/block_main_menu.php'; ?>
+		<hr/>
 	</header>
 	<main>
 		<article>
 			<? include_once $page_content; ?>
 		</article>
 	</main>
-	<hr/>
-	<? include_once 'modules/block_main_menu.php'; ?>
-	<hr/>
 <? include_once 'modules/site_footer.php'; ?>
