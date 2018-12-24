03d5fef23c7cd03b98b03d0a6e5d4cdd123921a2
Artem Smirnov <urpylka@gmail.com>
2018-12-24 18:10:22 +0300

    Local works

--- htdocs/config.php
+++ htdocs/config.php
@@ -1,15 +1,17 @@
 ﻿<?
     // Site settings
     $site_url = "http://robotic.lol";
+    // $site_url = "localhost";
     $site_name = "ROBOTIC";
 
     // Database settings
-    $host = "robotic.lol";
+    // $host = "robotic.lol";
+    $host = "db";
     $port = "3306";
     $login_mysql = "admin";
     $password_mysql = "test";
     $dbname = "database";
 
     //Developer settings
-    $DEBUG = FALSE
+    $DEBUG = FALSE;
 ?>
