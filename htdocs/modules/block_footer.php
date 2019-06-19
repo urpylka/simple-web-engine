0d055836471112003939ba7d6ffbbab344e23956
Artem Smirnov <urpylka@gmail.com>
2019-06-19 16:13:18 +0300

    Create block_footer.php

---
+++ htdocs/modules/block_footer.php
@@ -0,0 +1,35 @@
+<!-- <script type="text/javascript" src="js/some.js" /> -->
+<link rel="stylesheet" type="text/css" href="css/block_footer.css" />
+
+<div class="block-footer">
+    <div class="colft1">
+        <div class="name">Общайтесь с нами</div>
+        <ul>
+            <li><a href="http://vk.com/smirart"><img src="img/vk.png"><span>Вконтакте</span></a></li>
+            <li><a href="https://twitter.com/urpylka"><img src="img/tw.png"><span>Twitter</span></a></li>
+            <li><a href="https://www.youtube.com/user/urpylka"><img src="img/yo.png"><span>YouTube</span></a></li>
+        </ul>
+    </div>
+    <div class="colft2">
+        <div class="name">Быстрый доступ</div>
+        <ul class="menuft">
+            <li><a href="/director">Руководство</a></li>
+            <li><a href="/history">История</a></li>
+            <li><a href="/struktura">Структура</a></li>
+            <li><a href="/library">Библиотека</a></li>
+            <li><a href="/feedback">Обратная связь</a></li>
+        </ul>
+    </div>
+    <div class="colftbig">
+        <div class="name">Контактная информация</div>
+        <p style="margin-bottom: 3px;">Дежурный по кафедре:</p>
+        <span style="color:#000;">Тел.: (846) 2674422</span>
+        <p style="margin-bottom: 3px;">Начальник кафедры:</p>
+        <span style="color:#000;">Тел.: (846) 3345754</span>
+        <p style="margin-bottom: 3px;">Почтовый адрес:</p>
+        <span style="color:#000;">Россия, 443086, г. Самара, ул. Врубеля 27</span>
+    </div>
+    <div class="colft4">
+        <img height="170px" src="img/yo.png">
+    </div>
+</div>
