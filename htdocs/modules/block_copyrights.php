6749878d5bc446872e6025a613f615fee36d8d31
Artem Smirnov <urpylka@gmail.com>
2019-06-19 16:12:59 +0300

    Create block_copyrights.php

---
+++ htdocs/modules/block_copyrights.php
@@ -0,0 +1,21 @@
+<? // для удобства http://www.utilities-online.info/urlencode/ ?>
+<style>
+/* copyright */
+.copy{width:930px;
+	background-color:#f2f2f2;
+	height:28px;
+	padding-top: 14px;
+	padding-left:30px;
+	padding-right:30px;
+	color:#4e4a43;
+    font-size:13px;
+    margin: 0 !important;
+}
+.copy a {color:#4e4a43;text-decoration: underline;}
+.year {float:left;}
+.author {float:right;}
+</style>
+
+<script type="text/javascript">
+document.write(unescape('%3Cdiv%20class%3D%22copy%22%3E%0A%3Cdiv%20class%3D%22year%22%3E%A9%202001-2019%20SMIRART%3C/div%3E%0A%3Cdiv%20class%3D%22author%22%3EOpenSource%20on%20%3Ca%20href%3D%22https%3Agithub.com/urpylka/simple-web-engine%22%20target%3D%22_blank%22%3Egithub%3C/a%3E%3C/div%3E%0A%3C/div%3E%0A'));
+</script>
