730982670920a77e71f5aed89a8edcc48b4df529
Artem Smirnov <urpylka@gmail.com>
2019-06-19 13:45:50 +0300

    Rename quick_menu

--- htdocs/modules/template_main.php
+++ htdocs/modules/template_main.php
@@ -2,7 +2,7 @@
 	<header>
 		<? include_once 'modules/module_navbar.php'; ?>
 		<? include_once 'modules/block_promo.php'; ?>
-		<? include_once 'modules/block_quick-menu.php'; ?>
+		<? include_once 'modules/block_quick_menu.php'; ?>
 		<? include_once 'modules/block_site_map.php'; ?>
 	</header>
 	<main>
